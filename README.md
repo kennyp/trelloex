5ada20f17e10203c76e7acf700e2f1fbf5864473
Kenny Parnell <k.parnell@gmail.com>
2014-10-23 11:28:57 -0400

    Rename project to TrelloEx

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-ElixirTrello
-============
+TrelloEx
+========
 
 Summary
 -------
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ElixirTrello.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :elixir_trello,
+    [app: :trelloex,
      version: "0.0.1",
      elixir: "~> 1.0",
      deps: deps]
